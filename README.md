a8d02a585671c143f09a5788075ce6eb9aeeaa9e
ArtKV1 <arturkakharisov@yandex.ru>
2023-11-09 14:47:41 +0300

    Minor update 2.1.2

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # TelegramBot
-<p>Version 2.1.1</p>
+<p>Version 2.1.2</p>
 <p>All functions are working</p>
--- TelegramBot/TelegramBot/MessageHandler.cs
+++ TelegramBot/TelegramBot/MessageHandler.cs
@@ -134,7 +134,7 @@ namespace TelegramBot
 
                         await botClient.SendTextMessageAsync(
                                 chatId: chatId,
-                                text: "Вас привествует электронный помощник по поиску репетиторов.\n\nДанный бот работает в тестом режиме, если есть какие-то пожелания, то пишите @FindTutor_Support.",
+                                text: "Вас привествует электронный помощник по поиску репетиторов.\n\nЕсли здесь нет вашего предмета, то напишите сюда: @FindTutor_Support.",
                                 replyMarkup: inlineKeyboard,
                                 cancellationToken: cancellationToken);
                     }
